a871ed16d467d80d1478631cd464f633ae325e08
Aravind1704003 <aravind1704003@gmail.com>
2023-07-04 15:30:16 +0530

    mobile response 1

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
-import React from 'react';
-import { Helmet } from 'react-helmet';
-import Header from './Components/Header/Header';
-import TopContainer from './Components/TopContainer/TopContainer';
-import SkillContainer from './Components/SkillContainer/SkillContainer';
-import ProjectContainer from './Components/ProjectContainer/ProjectContainer';
-import Mydetails from './Components/Mydetails/Mydetails';
-import Contact from './Components/Contact/Contact';
-import Experience from './Components/Experience/Experience';
+import React from 'react'
+import Header from './Components/Header/Header'
+import TopContainer from './Components/TopContainer/TopContainer'
+import SkillContainer from './Components/SkillContainer/SkillContainer'
+import ProjectContainer from './Components/ProjectContainer/ProjectContainer'
+import Mydetails from './Components/Mydetails/Mydetails'
+import Contact from './Components/Contact/Contact'
+import Experience from './Components/Experience/Experience'
 
 const App = () => {
   return (
-    <div>
-      <Helmet>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Helmet>
 
-      <Header />
-      <TopContainer />
-      <Mydetails />
-      <SkillContainer />
-      <ProjectContainer />
-      <Experience />
-      <Contact />
-    </div>
-  );
+    <div>
+     
+   <Header />
+   <TopContainer />
+   <Mydetails />
+   <SkillContainer />
+   <ProjectContainer />
+   <Experience />
+   <Contact />
+   </div>
+  )
 }
 
-export default App;
+export default App
